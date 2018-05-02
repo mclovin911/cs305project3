7794f680a48ccd7de6cce14ada1fa62b1c24562f
JuneZho <38892180+JuneZho@users.noreply.github.com>
2018-05-01 21:37:18 -0400

    Add files via upload

--- Router.java
+++ Router.java
@@ -9,12 +9,13 @@ public class Router {
 
     Socket s;
     private boolean reverse;
+    private Address addr;
     private String ip;
     private int port;
     private Table t;
-    HashMap<String, Integer> DV = new HashMap<String, Integer>(); //this distance vector of this router
-    HashMap<String, Integer> distance = new HashMap<String, Integer>(); //this distance to neighbor
-    HashMap<String, HashMap<String, Integer>> neighborDV = new HashMap<String, HashMap<String, Integer>>();//the distance vector that this router received
+    HashMap<Address, Integer> DV = new HashMap<Address, Integer>(); //this distance vector of this router
+    HashMap<Address, Integer> distance = new HashMap<Address, Integer>(); //this distance to neighbor
+    HashMap<Address, HashMap<Address, Integer>> neighborDV = new HashMap<Address, HashMap<Address, Integer>>();//the distance vector that this router received
 
     public Router(String filename, boolean reverse) {
 
@@ -27,6 +28,9 @@ public class Router {
             s = new Socket(ip, port);
         } catch (Exception e) {
             e.printStackTrace();
+        }
+        if(s==null){
+            System.out.println("?????");
         }
         updateThread u = new updateThread(this, 10000);
         Thread uT = new Thread(u);
@@ -42,54 +46,54 @@ public class Router {
     }
 
     public boolean isReverse() {
-		return reverse;
-	}
+        return reverse;
+    }
 
-	public void setReverse(boolean reverse) {
-		this.reverse = reverse;
-	}
+    public void setReverse(boolean reverse) {
+        this.reverse = reverse;
+    }
 
-	public String getIp() {
-		return ip;
-	}
+    public Address getAddr() {
+        return addr;
+    }
 
-	public void setIp(String ip) {
-		this.ip = ip;
-	}
+    public void setAddr(Address a) {
+        this.addr = a;
+    }
 
-	public int getPort() {
-		return port;
-	}
+    public int getPort() {
+        return port;
+    }
 
-	public void setPort(int port) {
-		this.port = port;
-	}
+    public void setPort(int port) {
+        this.port = port;
+    }
 
-	public HashMap<String, Integer> getDV() {
-		return DV;
-	}
+    public HashMap<Address, Integer> getDV() {
+        return DV;
+    }
 
-	public void setDV(HashMap<String, Integer> dV) {
-		DV = dV;
-	}
+    public void setDV(HashMap<Address, Integer> dV) {
+        DV = dV;
+    }
 
-	public HashMap<String, Integer> getDistance() {
-		return distance;
-	}
+    public HashMap<Address, Integer> getDistance() {
+        return distance;
+    }
 
-	public void setDistance(HashMap<String, Integer> distance) {
-		this.distance = distance;
-	}
+    public void setDistance(HashMap<Address, Integer> distance) {
+        this.distance = distance;
+    }
 
-	public HashMap<String, HashMap<String, Integer>> getNeighborDV() {
-		return neighborDV;
-	}
+    public HashMap<Address, HashMap<Address, Integer>> getNeighborDV() {
+        return neighborDV;
+    }
 
-	public void setNeighborDV(HashMap<String, HashMap<String, Integer>> neighborDV) {
-		this.neighborDV = neighborDV;
-	}
+    public void setNeighborDV(HashMap<Address, HashMap<Address, Integer>> neighborDV) {
+        this.neighborDV = neighborDV;
+    }
 
-	public void readFile(String filename) {
+    public void readFile(String filename) {
 
         File f = new File(filename);
         String info = "";
@@ -98,12 +102,13 @@ public class Router {
             String[] data = s.nextLine().split(" ");
             ip = data[0];
             port = Integer.parseInt(data[1]);
+            setAddr(new Address(ip,port));
             int index = 0;
             while (s.hasNextLine()) {
 
                 String[] d = s.nextLine().split(" ");
                 info = d[0] + " " + d[1];
-                distance.put(info, Integer.parseInt(d[2]));
+                distance.put(new Address(d[0],Integer.parseInt(d[1])), Integer.parseInt(d[2]));
                 DV = distance; //initialize the distance vector. (the weight to known neighbor)
             }
 
@@ -115,13 +120,13 @@ public class Router {
 
 
     }
-    
-    public void putDV(String senderInfo, HashMap<String, Integer> tmp) {
-    	neighborDV.put(senderInfo, tmp);
+
+    public void putDV(Address senderInfo, HashMap<Address, Integer> tmp) {
+        neighborDV.put(senderInfo, tmp);
     }
-    
+
     public void pubInTable(String dest, String next) {
-    	t.put(dest, next);
+        t.put(dest, next);
     }
 
     public static void main(String args[]) {
@@ -131,7 +136,7 @@ public class Router {
         String s = "127.0.0.1 9877 1" + "\n" + "127.0.0.1 9876 1" + "\n" + "127.0.0.1 9874 1";
 
         try {
-            //r.s.send(s.getBytes(), "127.0.0.2", 9877);
+            r.s.send(s.getBytes(), "127.0.0.2", 9877);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -148,32 +153,33 @@ class updateThread implements Runnable{
         this.r= r;
     }
 
-   @Override
-   public void run() {
-       while (!Thread.currentThread().isInterrupted()) {
+    @Override
+    public void run() {
+        while (!Thread.currentThread().isInterrupted()) {
 
-           System.out.println("update !!!");
-           try {
-               Thread.sleep(gap);
-           } catch (InterruptedException e) {
-               e.printStackTrace();
-           }
-       }
-   }
-   
-   private void recalcDV() {
-	   
-	   //dist(s) = min of all neighbor i{dist(i->s)+dist(i)}
-	   
-	   
-	   
-	   //this dist to itself is always 0
-	   r.pubInTable(r.getIp(),r.getIp());
-	   
-	   
-	   
-	   
-   }
+            System.out.println("update !!!");
+            try {
+                Thread.sleep(gap);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void recalcDV() {
+
+        //dist(s) = min of all neighbor i{dist(i->s)+dist(i)}
+
+
+
+        //this dist to itself is always 0
+        r.pubInTable(r.getAddr().getIp(),r.getAddr().getIp());
+        //r.getDV().put();
+
+
+
+
+    }
 
 
 }
@@ -194,24 +200,25 @@ class receiveThread  implements Runnable {
         while (!Thread.currentThread().isInterrupted()) {
 
             try {
+                if(r.s!=null)
                 data = r.s.receive();
 
             } catch (Exception e) {
                 e.printStackTrace();
             }
             if (data != null) {
-                String info = data.getData().toString();
+                String info = new String(data.getData());
                 Scanner s = new Scanner(info);
-                HashMap<String, Integer> DV;
-                String firstL = s.nextLine();
-                DV = new HashMap<String, Integer>();
+                HashMap<Address, Integer> DV;
+                DV = new HashMap<Address, Integer>();
                 System.out.println("get msg");
                 while(s.hasNextLine()){
                     String i[] = s.nextLine().split(" ");
                     String str = i[0]+" "+i[1];
-                    DV.put(str,Integer.parseInt(i[2]));
+                    DV.put(new Address(i[0],Integer.parseInt(i[1])),Integer.parseInt(i[2].trim()));
                 }
-                r.putDV(firstL, DV); //put the DV sent by the neighbor to the neiborDV. 
+                System.out.println(data.getAddress().toString().split("/")[1]);
+                r.putDV(new Address(data.getAddress().toString().split("/")[1],data.getPort()), DV); //put the DV sent by the neighbor to the neiborDV.
 
             }
 
@@ -224,45 +231,45 @@ class receiveThread  implements Runnable {
 class readThread implements Runnable {
 
 
-        @Override
-            public void run() {
-                Scanner in = new Scanner(System.in);
-                System.out.println("Pls give commands\n");
-                while (in.hasNextLine()) {
-                    try {
-                        String command = in.nextLine();
-                        Scanner s = new Scanner(command);
-                        if (s.hasNext()) {
-                            String firstWord = s.next();
-                            switch (firstWord) {
-                                case ("PRINT"): {
+    @Override
+    public void run() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Pls give commands\n");
+        while (in.hasNextLine()) {
+            try {
+                String command = in.nextLine();
+                Scanner s = new Scanner(command);
+                if (s.hasNext()) {
+                    String firstWord = s.next();
+                    switch (firstWord) {
+                        case ("PRINT"): {
 
-                                    break;
-                                }
-                                case ("MSG"): {
-                                    String destIp = s.next();
-                                    String destPort = s.next();
-                                    String message = "";
-                                    if(s.hasNext()){
-                                        message += s.next();
-                                    }
-                                    while(s.hasNext()){
-                                        message += " " + s.next();
-                                    }
-
-                                }
-                                case ("CHANGE"): {
-                                    String destIp = s.next();
-                                    String destPort = s.next();
-                                    String weight = s.next();
-                                }
-                            }
+                            break;
                         }
+                        case ("MSG"): {
+                            String destIp = s.next();
+                            String destPort = s.next();
+                            String message = "";
+                            if(s.hasNext()){
+                                message += s.next();
+                            }
+                            while(s.hasNext()){
+                                message += " " + s.next();
+                            }
 
-                    } catch (Exception e) {
-                        System.out.println("Error, please try again");
+                        }
+                        case ("CHANGE"): {
+                            String destIp = s.next();
+                            String destPort = s.next();
+                            String weight = s.next();
+                        }
                     }
                 }
+
+            } catch (Exception e) {
+                System.out.println("Error, please try again");
             }
+        }
+    }
 
 }
